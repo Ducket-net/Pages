70c7d5a1d353297a0a9970f963da447c5ec99e6a
Wimell <99610715+Wimell@users.noreply.github.com>
2023-07-26 22:47:34 -0400

    Update script.js

--- staff/script.js
+++ staff/script.js
@@ -171,7 +171,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   async function fetchStaffData() {
     try {
-      const response = await fetch("/Staff/Staff.json");
+      const response = await fetch("/staff/staff.json");
       if (!response.ok) {
         throw new Error("Failed to fetch staff data.");
       }
@@ -193,7 +193,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   
   try {
-    const response = await fetch("/Staff/Staff.json");
+    const response = await fetch("/staff/staff.json");
     const data = await response.json();
     displayStaffList(data);
   } catch (error) {
